b9c4df784fd2a027c92f6f18d3dd027896fecb8c
Chris P <42878642+Griffen8280@users.noreply.github.com>
2022-09-01 16:57:28 -0400

    Update getlist.sh

--- build/getlist.sh
+++ build/getlist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O ../_pages/AppList.md https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/docs/AppList.md
+wget -O ./_pages/AppList.md https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/docs/AppList.md
